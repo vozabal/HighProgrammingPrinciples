4f0f14c60ccc4ba76d73506ab34ee90eac904241
vozabal <vozabal@users.noreply.github.com>
2015-11-16 15:08:57 +0100

    The class of the database was added

--- HighProgrammingPrinciples/main.cpp
+++ HighProgrammingPrinciples/main.cpp
@@ -1,9 +1,16 @@
 #include <QtCore/QCoreApplication>
 #include "sqlite3.h"
+#include <iostream>
+#include "Database.cpp"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
-	return a.exec();////
+	Database db;
+	db.Open_database();
+	db.Close_database();
+	return a.exec();
 }
