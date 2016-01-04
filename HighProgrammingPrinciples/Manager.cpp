92b7c8a2f731abd743f3604ee29c73558d4e5988
vozabal <vozabal@users.noreply.github.com>
2016-01-04 16:15:55 +0100

    Decreaces the interation number and edites the windows and linux path

--- HighProgrammingPrinciples/Manager.cpp
+++ HighProgrammingPrinciples/Manager.cpp
@@ -4,11 +4,11 @@
 Manager::Manager(string db_path, string boundaries_path)
 {
 #ifdef _WIN32
-	db_path = "Resources//direcnet.sqlite";
-	boundaries_path = "Resources//bounds.ini";
-#else
 	db_path = "Resources\\direcnet.sqlite";
 	boundaries_path = "Resources\\bounds.ini";
+#else
+	db_path = "Resources//direcnet.sqlite";
+	boundaries_path = "Resources//bounds.ini";
 #endif
 
 	IntervalLoader intervalLoader(boundaries_path);	// Initializes the intervalLoader
