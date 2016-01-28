d3897318745b0eefbf3baf4d41f691dc03432779
vozabal <vozabal@users.noreply.github.com>
2016-01-28 12:18:09 +0100

    manager was deleted

--- HighProgrammingPrinciples/main.cpp
+++ HighProgrammingPrinciples/main.cpp
@@ -104,7 +104,7 @@ void PrintUsage()
 		<< "and Simplex Optimization Algorithm created by Nelder and Mead.\n"
 		<< "The result (i.e. a table of calculated parameters and their fitnesses values for particular segments)\n"
 		<< "is printed to the OS console and possibly to an output file named \"result.txt\". If it is launched\n"
-		<< "without set up parameters the default will be used.\n\n"
+		<< "without set up parameters the default values will be used.\n\n"
 		<< "AUTHOR:  Miroslav Vozabal, University of West Bohemia, 2015/2016\n"
 		<< "---------------------------------------------------------------------------------------------------------"
 		<< endl
